a1a77cc33bb6b5acff69d49b5763e9494b94e455
bpaxton <brandon.paxton@aviture.us.com>
2023-07-13 12:43:02 -0500

    f

---
+++ HalfFinishedAppsIWillDefinatelyGetTo/SpringInitial/src/test/java/com/example/springintial/SpringIntialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
